35b570f7608231de00d84e439abb54ddbc22c5a5
auxpasot <derrien.margaux16@gmail.com>
2025-10-03 12:00:08 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,162 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.363464723279406
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.49610845375540435,
+          "pitch": 0.20453531746993292,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -0.43951736130838626,
+          "pitch": 0.21235832897471063,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 0.38646790016626653,
+        "pitch": 0.2523102298522115,
+        "fov": 1.363464723279406
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4449565691800572,
+          "pitch": 0.28745045917160894,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 0.02856399145518118,
+          "pitch": 0.24685674527612456,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 1.4953333090736756,
+          "pitch": 0.27434196793064736,
+          "rotation": 0,
+          "target": "2-3"
+        },
+        {
+          "yaw": 3.0787144922979817,
+          "pitch": 0.26983323107856627,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": -0.03621293624710198,
+        "pitch": 0.41784345910175347,
+        "fov": 1.2928590494984102
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.0439497644015496,
+          "pitch": 0.37306399682552893,
+          "rotation": 0,
+          "target": "1-2"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.3646760241137805,
+          "pitch": 0.3208115194870462,
+          "title": "Titre de l'oeuvre",
+          "text": "<div>Cartel explicatif</div>"
+        },
+        {
+          "yaw": -0.508763597479625,
+          "pitch": 0.16373015692657944,
+          "title": "Titre de l'oeuvre",
+          "text": "Cartel explicatif"
+        },
+        {
+          "yaw": 0.3945369226055284,
+          "pitch": 0.30561720291437666,
+          "title": "Titre de l'oeuvre",
+          "text": "Cartel explicatif"
+        },
+        {
+          "yaw": 1.4108958626414356,
+          "pitch": 0.2589136983110354,
+          "title": "Titre de l'oeuvre",
+          "text": "<div>Cartel explicatif</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
